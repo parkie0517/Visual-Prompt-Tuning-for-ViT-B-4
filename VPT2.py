bacf5c3b9e51f1a09c5d555b4631ab41ec0e3d64
parkie0517 <parkie0517@gmail.com>
2024-01-27 07:17:37 +0000

    Add VPT2.py file and add comments

---
+++ VPT2.py
@@ -0,0 +1,8 @@
+"""
+This is a Visual Prompt Tuning practice created by Heejun Park.
+A ViT-B/16 pre-trained on ImageNet21k is going to be used.
+It will be fine-tuned using a prompt tuning method.
+The dataset required for fine-tuning will be CIFAR-10.
+Let's begin!  
+"""
+
