4508dc0be38f0bb5886587d225151d6039c028bd
parkie0517 <parkie0517@gmail.com>
2024-01-28 05:04:30 +0000

    Modify pre-trained model using different arguments

--- check.py
+++ check.py
@@ -2,12 +2,26 @@ import timm
 import inspect
 
 # Load the Vision Transformer model
-model = timm.create_model('vit_base_patch16_224', pretrained=True)
+model = timm.create_model('vit_base_patch16_224', 
+                          img_size=32, 
+                          patch_size=4, 
+                          num_classes=10, 
+                          pretrained=True,
+                          )
 
 # Get the forward method of the model
-forward_method = model.forward_features
+#forward_method = model.__init__
 
 # Use inspect to get the source code of the forward method
-forward_source_code = inspect.getsource(forward_method)
+#forward_source_code = inspect.getsource(forward_method)
 
-print(forward_source_code)
+#print(forward_source_code)
+#print(model)
+
+# Iterate through all named modules
+for module_name, module in model.named_modules():
+    print(f"Module: {module_name}")
+    
+    # Iterate through all named parameters of the module
+    for param_name, param in module.named_parameters():
+        print(f"\tParameter: {param_name}, Shape: {param.shape}")
