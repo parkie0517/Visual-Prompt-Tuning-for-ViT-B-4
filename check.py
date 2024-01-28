d2f516888ddc1492b30d3684aa2ea06e3db546f2
parkie0517 <parkie0517@gmail.com>
2024-01-28 03:23:42 +0000

    Check how model's function works

--- check.py
+++ check.py
@@ -5,7 +5,7 @@ import inspect
 model = timm.create_model('vit_base_patch16_224', pretrained=True)
 
 # Get the forward method of the model
-forward_method = model.forward_features
+forward_method = model
 
 # Use inspect to get the source code of the forward method
 forward_source_code = inspect.getsource(forward_method)
